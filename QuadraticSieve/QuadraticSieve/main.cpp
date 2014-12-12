b46404f9665e988516b49b8973e18d618d6ded00
jaj2610 <jaj2610@gmail.com>
2014-12-12 06:19:21 -0500

    Adding more starter files

---
+++ QuadraticSieve/QuadraticSieve/main.cpp
@@ -0,0 +1,19 @@
+// Author: Jacob Jones
+// Class: Cmpsc467 Fall 2014 Penn State - UP
+// Date: 12/12/2014
+// Description: Implementation of Quadratic Sieve for the 15 digit number: 135291536006657
+
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <math.h>
+#include "BigInteger.h"
+#include "functions.h"
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+
+
+	return 0;
+}
